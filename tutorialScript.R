26a45beab77a1f11a5ed811d0bd900ed288719be
andytinkham <andy@tinkham.org>
2017-07-08 00:10:53 -0500

    Kaggle Submission #2 - predict all women will survive

--- tutorialScript.R
+++ tutorialScript.R
@@ -9,9 +9,15 @@ library(readr)
 train <- read.csv("data/train.csv")
 test <- read.csv("data/test.csv")
 
-# Most people died on the Titanic. Start with an assumption that all the test 
-# data people did as well.
-test$Survived <- rep(0, 418)
+# Look at proportions of men and women who survived
+# prop.table(table(train$Sex, train$Survived),1)
+
+# Start with assumption everyone dies (Correctness = 0.62679)
+test$Survived <- 0
+
+# Titanic famous for saving women and children first - refine to say all women
+# survived (Correctness = 0.76555)
+test$Survived[test$Sex == "female"] <- 1
 
 submit <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
-write.csv(submit, file = "submissions/theyallperish.csv", row.names = FALSE)
+write.csv(submit, file = "submissions/womenallsurvive.csv", row.names = FALSE)
