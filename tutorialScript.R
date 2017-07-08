756215b1d8dc8d10e3e2294e2402213e34d3cc1a
andytinkham <andy@tinkham.org>
2017-07-07 23:56:44 -0500

    First submission - everyone in test set dies

--- tutorialScript.R
+++ tutorialScript.R
@@ -1,9 +1,17 @@
-# Built following tutorial at http://trevorstephens.com/kaggle-titanic-tutorial/getting-started-with-r/
+# Built following tutorial at 
+# http://trevorstephens.com/kaggle-titanic-tutorial/getting-started-with-r/
 
 setwd("~/Rprojects/kaggle-titanic/")
 
 library(readr)
 
 # Pull in the datasets
-train <- read_csv("data/train.csv")
-test <- read_csv("data/test.csv")
+train <- read.csv("data/train.csv")
+test <- read.csv("data/test.csv")
+
+# Most people died on the Titanic. Start with an assumption that all the test 
+# data people did as well.
+test$Survived <- rep(0, 418)
+
+submit <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
+write.csv(submit, file = "submissions/theyallperish.csv", row.names = FALSE)
