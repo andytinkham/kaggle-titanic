35b1936f6100388f6e45f03ff7005a113fcbc39d
andytinkham <andy@tinkham.org>
2017-07-07 23:35:44 -0500

    Read in datasets

--- tutorialScript.R
+++ tutorialScript.R
@@ -1,3 +1,9 @@
 # Built following tutorial at http://trevorstephens.com/kaggle-titanic-tutorial/getting-started-with-r/
 
 setwd("~/Rprojects/kaggle-titanic/")
+
+library(readr)
+
+# Pull in the datasets
+train <- read_csv("data/train.csv")
+test <- read_csv("data/test.csv")
